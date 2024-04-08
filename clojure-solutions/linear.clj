37146f919279d8257e4d7830b536a86997cc9fa0
Andrew Shustrov <a.shustrov5@gmail.com>
2024-04-08 18:14:04 +0300

    Linear + Cuboid

---
+++ clojure-solutions/linear.clj
@@ -0,0 +1,42 @@
+(defn operation [f]
+  (fn [a b]
+    (apply mapv f a b)))
+
+(def v+ (operation +))
+(def v- (operation -))
+(def v* (operation *))
+(def vd (operation /))
+
+(defn scalar [v s]
+  (reduce + (v* v s)))
+
+(defn vect [[ax ay az] [bx by bz]]
+  (vector
+    (- (* ay bz) (* az by))
+    (- (* az bx) (* ax bz))
+    (- (* ax by) (* ay bx))))
+
+(defn v*s [v s]
+  (mapv (fn [e] (* e s)) v))
+
+(def m+ (operation v+))
+(def m- (operation v-))
+(def m* (operation v*))
+(def md (operation vd))
+
+(defn m*s [m s]
+  (mapv (fn [v] (v*s v s)) m))
+
+(defn m*v [m v]
+  (mapv (partial scalar v) m))
+
+(defn transpose [m]
+  (apply mapv vector m))
+
+(defn m*m [m1 m2]
+  (mapv (partial m*v (transpose m2)) m1))
+
+(def c+ (operation m+))
+(def c- (operation m-))
+(def c* (operation m*))
+(def cd (operation md))

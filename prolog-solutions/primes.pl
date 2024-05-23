f26b5682be9c411aa866194359e8431ccd5c8b9b
Andrew Shustrov <a.shustrov5@gmail.com>
2024-05-23 12:05:13 +0300

    save

--- prolog-solutions/primes.pl
+++ prolog-solutions/primes.pl
@@ -9,37 +9,40 @@ composite(N) :- N > 1, \+ prime(N).
 
 
 nth_prime(N, P, Pos, Cur) :-
-    prime(Cur) ->
-    ( 
-  		Pos =:= N 
-  		-> 
-  		P is Cur
-  		;
-  		NextPos is Pos + 1,
-  		Next is Cur + 1,
-  		nth_prime(N, P, NextPos, Next)
-    )
-    ; 
+    prime(Cur),
     (
-    	Next is Cur + 1,
-    	nth_prime(N, P, Pos, Next)
+        Pos is N
+        ->
+        P is Cur
+        ;
+        NextPos is Pos + 1,
+        Next is Cur + 1,
+        nth_prime(N, P, NextPos, Next)
     ).
+
+nth_prime(N, P, Pos, Cur) :-
+    composite(Cur),
+    Next is Cur + 1,
+    nth_prime(N, P, Pos, Next).
 
 nth_prime(N, P) :- nth_prime(N, P, 1, 2).
 
 
 prime_divisors(1, Divisors, Divisors, _).
+% :NOTE: лишние аргументы
 prime_divisors(N, Divisors, List, Cur) :-
     N > 1,
-    ( 
+    (
         N mod Cur =:= 0
-    	->
+        ->
         N_Div is N // Cur,
-        append(List, [Cur], ListUpd),
-        prime_divisors(N_Div, Divisors, ListUpd, Cur)
-    	; 
-    	Next is Cur + 1,
+        prime_divisors(N_Div, Divisors, [Cur | List], Cur)
+        ;
+        Next is Cur + 1,
         prime_divisors(N, Divisors, List, Next)
     ).
 
-prime_divisors(N, Divisors) :- prime_divisors(N, Divisors, [], 2).
+% :NOTE: prime_divisors(N, [2,2,2,3])
+prime_divisors(N, Divisors) :-
+    prime_divisors(N, ReversedDivisors, [], 2),
+    reverse(ReversedDivisors, Divisors).
